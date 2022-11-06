59ca89b42c64f24246e466a4f93305abfbf60946
Bale Lin <lin.yu.shiang@gmail.com>
2022-11-07 01:13:00 +0800

    TT

--- app/src/main/java/com/kuangli/ip/FirstFragment.kt
+++ app/src/main/java/com/kuangli/ip/FirstFragment.kt
@@ -10,6 +10,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.kuangli.ip.databinding.FragmentFirstBinding
+import java.net.Inet4Address
+import java.net.NetworkInterface
+import java.net.SocketException
 
 
 /**
@@ -42,12 +45,45 @@ class FirstFragment : Fragment() {
         }
         layoutWifi()
     }
+
     private fun layoutWifi() {
         val context: Context = requireContext().applicationContext
         val wm = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
         val ip: String =
             Formatter.formatIpAddress(wm.connectionInfo.ipAddress)
-        _binding!!.ip.text = "IP: $ip"
+        _binding!!.ip.text = "IP: $ip \r\n ${getLocalIpAddress()} \r\n ${wifi3()}"
+    }
+
+    fun getLocalIpAddress(): String? {
+        try {
+            val en = NetworkInterface.getNetworkInterfaces()
+            while (en.hasMoreElements()) {
+                val intf = en.nextElement()
+                val enumIpAddr = intf.inetAddresses
+                while (enumIpAddr.hasMoreElements()) {
+                    val inetAddress = enumIpAddr.nextElement()
+                    if (!inetAddress.isLoopbackAddress && inetAddress is Inet4Address) {
+                        return inetAddress.getHostAddress()
+                    }
+                }
+            }
+        } catch (ex: SocketException) {
+            ex.printStackTrace()
+        }
+        return null
+    }
+
+    fun wifi3(): String {
+        val wifiMan = this.requireContext().getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val wifiInf = wifiMan.connectionInfo
+        val ipAddress = wifiInf.ipAddress
+        return String.format(
+            "%d.%d.%d.%d",
+            ipAddress and 0xff,
+            ipAddress shr 8 and 0xff,
+            ipAddress shr 16 and 0xff,
+            ipAddress shr 24 and 0xff
+        )
     }
 
     override fun onDestroyView() {
