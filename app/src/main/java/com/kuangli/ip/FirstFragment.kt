e3eab14062cd8974f12e5ae9c96a231a6ca64faa
Bale Lin <lin.yu.shiang@gmail.com>
2022-11-07 01:04:40 +0800

    TT

---
+++ app/src/main/java/com/kuangli/ip/FirstFragment.kt
@@ -0,0 +1,57 @@
+package com.kuangli.ip
+
+import android.content.Context
+import android.net.wifi.WifiManager
+import android.os.Bundle
+import android.text.format.Formatter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.kuangli.ip.databinding.FragmentFirstBinding
+
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        layoutWifi()
+    }
+    private fun layoutWifi() {
+        val context: Context = requireContext().applicationContext
+        val wm = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val ip: String =
+            Formatter.formatIpAddress(wm.connectionInfo.ipAddress)
+        _binding!!.ip.text = "IP: $ip"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
